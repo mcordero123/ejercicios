fadd84acbfa3dafc2287943e87084bb521dadee1
Miguel <mcordero@colegioelbelloto.com>
2025-05-14 16:36:57 -0400

    Tercer commit

---
+++ arreglos.js
@@ -0,0 +1,22 @@
+/*
+let arreglo = ["rojo", "amarillo", "azul ", "morado ", "celeste "];
+alert(arreglo);
+
+for (let i = 0; i<5 ; i++){
+    alert(arreglo[i]);
+}
+
+let numeros = [3, 5, 4, 7, 9];
+let suma = 0;
+
+for (let i = 0; i < numeros.length; i++){
+    /* suma = suma + numeros[i]; */
+   /* suma += numeros[i];
+}
+alert(suma); 
+
+*/ 
+function saludo(nombre){
+    alert("Hola " + nombre);
+}
+saludo("Felipe");
